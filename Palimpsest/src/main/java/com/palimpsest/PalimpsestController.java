640b25325e14694904e895f0c19ee7f43d8b5066
kurocifer <draculemihawk232@gmail.com>
2025-05-09 05:37:02 +0100

    Display Single Text Copy in UI #7

--- Palimpsest/src/main/java/com/palimpsest/PalimpsestController.java
+++ Palimpsest/src/main/java/com/palimpsest/PalimpsestController.java
@@ -2,32 +2,36 @@ package com.palimpsest;
 
 import com.palimpsest.model.TextCopy;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-
 import java.util.logging.Logger;
 
 public class PalimpsestController {
     private static final Logger LOGGER = Logger.getLogger(PalimpsestController.class.getName());
     private final TextCopy dummyCopy = new TextCopy("test", "12:00 PM");
 
-    @FXML
-    private Label text1;
-    @FXML
-    private Label time1;
+    @FXML private Label text1;
+    @FXML private Label time1;
+    @FXML private Button copy1;
 
     @FXML
-    public void copy() {
-        LOGGER.info("Copy button clicked - testing dummy TextCopy: text=" + dummyCopy.getText() + ", timestamp=" + dummyCopy.getTimestamp());
-        dummyCopy.copyToClipboard();
-        LOGGER.info("Dummy text copied to clipboard: " + dummyCopy.getText());
+    public void initialize() {
+        text1.setText(dummyCopy.getText());
+        time1.setText(dummyCopy.getTimestamp());
+        copy1.setOnAction(event -> {
+            LOGGER.info("Copy1 button clicked - copying: text=" + dummyCopy.getText());
+            dummyCopy.copyToClipboard();
+            LOGGER.info("Copied to clipboard: " + dummyCopy.getText());
+        });
     }
 
     @FXML
     public void copy1() {
-        TextCopy text1Copy = new TextCopy(text1.getText(), time1.getText());
+        LOGGER.info("Other copy button clicked - placeholder");
+    }
 
-        text1Copy.copyToClipboard();
-        LOGGER.info("Copy button clicked - testing dummy TextCopy: text=" + dummyCopy.getText() + ", timestamp=" + dummyCopy.getTimestamp());
-        LOGGER.info("Dummy text copied to clipboard: " + dummyCopy.getText());
+    @FXML
+    public void copy() {
+        LOGGER.info("Other copy button clicked - placeholder");
     }
 }
