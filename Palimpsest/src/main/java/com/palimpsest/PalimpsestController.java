e96a63bb188690b3f49a2bb77110240734ee1cdc
kurocifer <draculemihawk232@gmail.com>
2025-05-09 05:48:24 +0100

    Create List for Text Copies #8 Open

--- Palimpsest/src/main/java/com/palimpsest/PalimpsestController.java
+++ Palimpsest/src/main/java/com/palimpsest/PalimpsestController.java
@@ -2,32 +2,49 @@ package com.palimpsest;
 
 import com.palimpsest.model.TextCopy;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class PalimpsestController {
     private static final Logger LOGGER = Logger.getLogger(PalimpsestController.class.getName());
+    private final List<TextCopy> textCopies = new ArrayList<>(10);
     private final TextCopy dummyCopy = new TextCopy("test", "12:00 PM");
 
     @FXML private Label text1;
     @FXML private Label time1;
-    @FXML private Button copy1;
 
     @FXML
     public void initialize() {
+        // Set row 1 from dummy (Issue #12)
         text1.setText(dummyCopy.getText());
         time1.setText(dummyCopy.getTimestamp());
-        copy1.setOnAction(event -> {
-            LOGGER.info("Copy1 button clicked - copying: text=" + dummyCopy.getText());
-            dummyCopy.copyToClipboard();
-            LOGGER.info("Copied to clipboard: " + dummyCopy.getText());
-        });
+
+        // Test list with 3 dummies
+        addTextCopy(new TextCopy("copy3", "12:03 PM"));
+        addTextCopy(new TextCopy("copy2", "12:02 PM"));
+        addTextCopy(new TextCopy("copy1", "12:01 PM"));
+
+        // Log list to verify
+        for (int i = 0; i < textCopies.size(); i++) {
+            TextCopy copy = textCopies.get(i);
+            LOGGER.info("List index " + i + ": text=" + copy.getText() + ", timestamp=" + copy.getTimestamp());
+        }
+    }
+
+    public void addTextCopy(TextCopy textCopy) {
+        textCopies.add(0, textCopy);
+        if (textCopies.size() > 10) {
+            textCopies.remove(10);
+        }
     }
 
     @FXML
     public void copy1() {
-        LOGGER.info("Other copy button clicked - placeholder");
+        LOGGER.info("Copy1 button clicked - copying: text=" + dummyCopy.getText());
+        dummyCopy.copyToClipboard();
+        LOGGER.info("Copied to clipboard: " + dummyCopy.getText());
     }
 
     @FXML
